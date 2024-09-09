217060884402cf03f3c611958cee5b4bffce68e2
dheerajup98 <dheerajup98@gmail.com>
2024-09-09 12:35:16 +0530

    first py

---
+++ firstProgram.py
@@ -0,0 +1,25 @@
+print("hello python");
+
+# variables
+
+name = "dheeraj upadhyay";
+age = 26;
+isActive = True;
+nothing = None;
+
+print("my name is :", name);
+print("my age is :", age);
+print("is active :", isActive);
+print("it's nothing :", nothing);
+
+print(type (name));
+
+print(type (age));
+
+print(type (isActive));
+
+print(type (nothing));
+
+# Data Types
+# Int, float, double, none, boolean, string
+
