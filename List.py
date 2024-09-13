d2cf30dfeac444575ac29f0f55101495692ff2fd
dheerajup98 <dheerajup98@gmail.com>
2024-09-13 11:37:45 +0530

    list

---
+++ List.py
@@ -0,0 +1,31 @@
+# print("list in python")
+
+# list is similar to array data structure this is built in python in list we store any data types values.
+
+myList = ["dheeraj", 26, "ballia"]
+
+# print(myList[2])
+
+myList.append("delhi");
+
+# print(myList);
+
+myList2 = [25,98,45,11,7,32];
+
+myList2.sort()
+# print(myList2)
+
+myList2.sort(reverse=True)
+# print(myList2)
+
+myList2.remove(45)
+print(myList2)
+
+# myList2.index(1)
+# print(myList2)
+
+myList2.insert(1,88)
+print(myList2)
+
+myList2.pop(2)
+print(myList2)
